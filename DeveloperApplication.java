8fca7cf9b8bca6e30c0fd8573ea75b09f724ceb0
madhukiranreddy <kiranreddymadhu@gmail.com>
2025-07-06 23:17:41 +0530

    1 one

---
+++ DeveloperApplication.java
@@ -0,0 +1,9 @@
+package com.example.developer;
+
+public class DeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperApplication.class, args);
+	}
+
+}
